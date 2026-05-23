ae3bbb0769860ccbd3e0cfe6a46ebbb1e3c048f2
Chinnapogula Esther <chinnapogulaesther4@gmail.com>
2026-05-23 12:24:55 +0530

    ✅ Add solution for: High-Value Product Inventory Report - SQL

---
+++ exercises/HighValueProductInventoryReportSQL.sql
@@ -0,0 +1,13 @@
+-- Write a SQL query below --
+SELECT 
+    name, 
+    ROUND(price,2) AS price,stock
+    stock
+FROM 
+    products
+WHERE 
+    category_id = 4
+    AND price > 50.00
+    AND stock > 0
+ORDER BY 
+    price DESC;
